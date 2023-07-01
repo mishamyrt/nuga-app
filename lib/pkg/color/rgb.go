d3e23cf171d0e7742c6260587b060d22ac46eef8
mishamyrt <misha@myrt.co>
2023-07-01 13:31:11 +0300

    Simplify color

--- lib/pkg/color/rgb.go
+++ lib/pkg/color/rgb.go
@@ -2,19 +2,10 @@ package color
 
 type RGB struct {
 	R, G, B uint8
-	random  bool
-}
-
-func (c *RGB) IsRandom() bool {
-	return c.random
-}
-
-var Random = RGB{
-	random: true,
 }
 
 func New(r, g, b uint8) RGB {
 	return RGB{
-		r, g, b, false,
+		r, g, b,
 	}
 }
