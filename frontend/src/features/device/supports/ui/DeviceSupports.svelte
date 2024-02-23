0c2050ffab462c16ecada53a5949e103fafb5f18
mishamyrt <misha@myrt.co>
2024-02-23 17:55:20 +0300

    fix: remove fsd wrapper (affects layout)

--- frontend/src/features/device/supports/ui/DeviceSupports.svelte
+++ frontend/src/features/device/supports/ui/DeviceSupports.svelte
@@ -1,21 +1,11 @@
 <script lang="ts">
-  import { fsd } from 'feature-sliced-svelte'
-
   import { type Supports, supportsStore } from '$entities/device'
 
   export let capability: keyof Supports
 </script>
 
-<div class="supports" use:fsd={'features/DeviceSupports'}>
-  {#if $supportsStore[capability]}
-    <slot />
-  {:else}
-    <slot name="not-supported" />
-  {/if}
-</div>
-
-<style lang="scss">
-  .supports {
-    display: contents;
-  }
-</style>
+{#if $supportsStore[capability]}
+  <slot />
+{:else}
+  <slot name="not-supported" />
+{/if}
