0a3a844058e325526b78702488b9a941885aaf38
mishamyrt <misha@myrt.co>
2024-02-28 21:39:27 +0300

    fix: init sidebar menu with correct value

--- frontend/src/entities/app/ui/AppSidebarMenu.svelte
+++ frontend/src/entities/app/ui/AppSidebarMenu.svelte
@@ -2,11 +2,13 @@
   import { SidebarMenu } from '@naco-ui/svelte'
   import { fsd } from 'feature-sliced-svelte'
 
+  import { activePage } from '$shared/model'
+
   import { AppIconBulb, AppIconGear, AppIconKeyboard } from './icons'
 </script>
 
 <div use:fsd={'entities/AppSidebarMenu'}>
-  <SidebarMenu size="l" on:change items={[
+  <SidebarMenu value={$activePage} size="l" on:change items={[
     { title: 'Lights', value: 'lights', icon: AppIconBulb },
     { title: 'Keys', value: 'keys', icon: AppIconKeyboard },
     { title: 'Device', value: 'device', icon: AppIconGear }
