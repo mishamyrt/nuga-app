9cefaba873497f62d0af86786f240a7b9a3de93f
mishamyrt <misha@myrt.co>
2024-03-02 22:57:23 +0300

    fix: disable state restore on unsupported devices

--- frontend/src/widgets/ui/DeviceSettings.svelte
+++ frontend/src/widgets/ui/DeviceSettings.svelte
@@ -1,9 +1,9 @@
 <svelte:options immutable={true}/>
 <script lang="ts">
   import { FormGroup, FormRow, Stack } from '@naco-ui/svelte'
-  import { fsd } from 'feature-sliced-svelte'
 
   import { restoringStateStore } from '$entities/device'
+  import { supportsKeyStore } from '$entities/keys'
   import {
     OSModeSelect,
     StateBackupButton,
@@ -12,30 +12,28 @@
   } from '$features/device'
 
   $: isRestoring = $restoringStateStore
+  $: supportsKeys = $supportsKeyStore
 </script>
-<div use:fsd={'widgets/DeviceSettings'}>
-  <FormGroup title="Configuration">
-    <FormRow title="System mode">
-      <OSModeSelect />
-    </FormRow>
-    <FormRow align="top" title="State" subtitle="The complete state of the device: lights and keys is saved to the file.
-    The state can be restored only on the same model of the keyboard.
-    Files made in different program versions may be incompatible.">
-      <Stack fullHeight align="start" justify="end" direction="horizontal" gap="s">
-        <StateBackupButton>Save...</StateBackupButton>
-        <StateRestoreButton disabled={isRestoring}>Load...</StateRestoreButton>
-      </Stack>
-    </FormRow>
-  </FormGroup>
-  <div class="restore">
-    <StateRestoreDefaultButton disabled={isRestoring}>Restore default</StateRestoreDefaultButton>
-  </div>
+<FormGroup title="Configuration">
+  <FormRow title="System mode">
+    <OSModeSelect />
+  </FormRow>
+  <FormRow align="top" title="State" subtitle="The complete state of the device: lights and keys is saved to the file.
+  The state can be restored only on the same model of the keyboard.
+  Files made in different program versions may be incompatible.">
+    <Stack fullHeight align="start" justify="end" direction="horizontal" gap="s">
+      <StateBackupButton>Save...</StateBackupButton>
+      <StateRestoreButton disabled={isRestoring}>Load...</StateRestoreButton>
+    </Stack>
+  </FormRow>
+</FormGroup>
+<div class="restore">
+  <StateRestoreDefaultButton disabled={!supportsKeys || isRestoring}>Restore default</StateRestoreDefaultButton>
 </div>
 
 <style lang="scss">
   .restore {
     display: flex;
     justify-content: flex-end;
-    margin-top: var(--space-m);
   }
 </style>
