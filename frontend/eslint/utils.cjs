79c9530942a4c328f8939a8370c909ab73d1feee
mishamyrt <misha@myrt.co>
2024-03-05 00:45:06 +0300

    chore: add fsd layers linting

---
+++ frontend/eslint/utils.cjs
@@ -0,0 +1,30 @@
+const FS_LAYERS = [
+  'app',
+  'processes',
+  'pages',
+  'widgets',
+  'features',
+  'entities',
+  'shared'
+]
+
+const FS_SEGMENTS = ['ui', 'model', 'lib', 'api', 'config', 'assets']
+
+/**
+ * @param {string} layer
+ * @returns {string[]}
+ */
+const getLowerLayers = (layer) => FS_LAYERS.slice(FS_LAYERS.indexOf(layer) + 1)
+
+/**
+ * @param {string} layer
+ * @returns {string[]}
+ */
+const getUpperLayers = (layer) => FS_LAYERS.slice(0, FS_LAYERS.indexOf(layer))
+
+module.exports = {
+  FS_LAYERS,
+  FS_SEGMENTS,
+  getLowerLayers,
+  getUpperLayers
+}
