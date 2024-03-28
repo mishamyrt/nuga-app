4cff39db567e63c9598daa7bfa877a730b8bebc1
mishamyrt <misha@myrt.co>
2024-03-28 11:56:37 +0300

    refactor: clean up same action checker

--- ui/src/entities/keys/lib/action.ts
+++ ui/src/entities/keys/lib/action.ts
@@ -2,11 +2,10 @@ import type { KeyAction } from '../model/types'
 
 export function isSameAction (a: KeyAction, b: KeyAction): boolean {
   let isModifiersDiffers = false
-  // NOTE: False positive
-  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-  if ((a.modifiers && !b.modifiers) || (!a.modifiers && b.modifiers)) {
+  if (Boolean(a.modifiers) !== Boolean(b.modifiers)) {
     return false
-  } else if (a.modifiers && b.modifiers) {
+  }
+  if (a.modifiers && b.modifiers) {
     isModifiersDiffers = !(
       a.modifiers.ctrl === b.modifiers.ctrl &&
       a.modifiers.shift === b.modifiers.shift &&
