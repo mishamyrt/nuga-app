467d90d18614413ce75f02804ba92b1621196cdb
mishamyrt <misha@myrt.co>
2024-03-28 10:34:52 +0300

    feat: add StoreDescription debug component

---
+++ ui/src/shared/ui/StoreDescription/StoreDescription.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { FormGroup, FormRow } from '@naco-ui/svelte'
+  import type { Store } from 'effector'
+
+  export let store: Store<any>
+
+  $: value = JSON.stringify($store, null, 2)
+</script>
+
+<FormGroup>
+  <FormRow>
+    <div class="store-description">
+      {value}
+    </div>
+  </FormRow>
+</FormGroup>
+
+<style lang="scss">
+  .store-description {
+    font-family: ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas, Liberation Mono, monospace;
+    color: var(--color-content-primary);
+    font-size: 12px;
+  }
+</style>
